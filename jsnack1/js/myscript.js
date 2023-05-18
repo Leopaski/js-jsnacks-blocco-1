f23f58e62864e6071b4368c8e742630416ecec13
Leopaski <leonardo.pascale@gmail.com>
2023-05-18 17:11:33 +0200

    snack1

--- jsnack1/js/myscript.js
+++ jsnack1/js/myscript.js
@@ -1,7 +1,7 @@
-let listNumber =[n1,n2,n3,n4,n5,n6,n7,n8,n9,n10];
+const listNumber =[1,2,3,4,5,6,7,8,9,10];
 let finalNumber=0;
 for (let i=0;i<listNumber.length;i++ ){
-   listNumber[i]= parsInt(prompt('Inserisci un numero da sommare'));
+   listNumber[i]= parseInt(prompt('inserisci un numero che vuoi sommare'));
    finalNumber= listNumber[i]+finalNumber;
 }
 console.log(finalNumber);
