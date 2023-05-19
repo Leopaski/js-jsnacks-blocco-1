4297457265d55d71654b02fc58fb9399c00d107c
Leopaski <leonardo.pascale@gmail.com>
2023-05-19 10:50:23 +0200

    correzione snack 4

--- jsnack4/js/myscript.js
+++ jsnack4/js/myscript.js
@@ -1,2 +1,9 @@
 let number = prompt('inserisci un numero di 4 cifre')
+if(!(number>999 && number < 9999)) {
+    number= prompt('inserisci un numero di 4 cifre');
+}
+let sum=0;
+for (let i=0; i < number.length; i++){
+    sum += parseInt(number.charAt(i));
+}
 console.log(number)
